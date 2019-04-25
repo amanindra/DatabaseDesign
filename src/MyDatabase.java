0991cfcfd2aef388e2f4af2c6bc9c3cf0932f1e1
Manindra <a.manindra1995@gmail.com>
2019-04-25 18:43:14 -0500

    PageSize changed to long

--- src/MyDatabase.java
+++ src/MyDatabase.java
@@ -21,7 +21,7 @@ public class MyDatabase {
 	protected static RandomAccessFile dbColumnFile;
 	protected static RandomAccessFile dbMasterTableFile;
 	protected static ArrayList<String> history;
-	static long pageSize = 512;
+	static long pageSize = 512L;
 	static Scanner scanner = new Scanner(System.in).useDelimiter(";");
 
 	private static void buildDBColumnTable(RandomAccessFile mDBColumnFile,
