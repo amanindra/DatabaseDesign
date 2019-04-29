24482fbb3ea43a677ea3d327aa25e5b89370de46
powerlich227 <yemingxiao1990@gmail.com>
2019-04-28 19:39:15 -0500

    updated by Mingxiao

--- src/WelcomePage.java
+++ src/WelcomePage.java
@@ -1,16 +1,15 @@
 public class WelcomePage {
 	
-	private static final String version = "v0.1.0";
-	private static final String copyright = "@2019 Arpitha Manindra MingxioYe Nileshwari Shivani";
+	private static final String version = "v1.0.0";
+	private static final String copyright = "@2019 Arpitha, Manindra, Mingxiao, Nileshwari, Shivani";
 
 	public static void splashScreen() {
-		System.out.println(Utility.repeat("-", 80));
-		System.out.println("Welcome to MyDatabase"); // Display the string.
+		System.out.println(Utility.displayLine("*", 70));
+		System.out.println("Welcome to MyDatabase");
 		System.out.println("MyDatabase Version " + version);
 		System.out.println(copyright);
-		System.out
-				.println("\nType \"help;\" to display the supported commands of MyDatabase.");
-		System.out.println(Utility.repeat("-", 80));
+		System.out.println("\nType \"help;\" to display the supported commands of MyDatabase.\n");
+		System.out.println(Utility.displayLine("*", 70));
 	}
 
 	public static void displayVersion() {
