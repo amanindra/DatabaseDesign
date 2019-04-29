b3e1487f76a25a85dcf2e6d2b865704fa8d6d1d8
powerlich227 <yemingxiao1990@gmail.com>
2019-04-28 19:40:14 -0500

    updated by Mingxiao

--- src/Utility.java
+++ src/Utility.java
@@ -21,31 +21,38 @@ public class Utility {
 	private static final String DATETIME = "datetime";
 	private static final String DATE = "date";
 
-	public static String repeat(String s, int num) {
-		String a = "";
-		for (int i = 0; i < num; i++) {
-			a += s;
+	public static String displayLine(String string, int number) {
+		String str = "";
+		for (int i = 0; i < number; i++) {
+			str += string;
 		}
-		return a;
+		return str;
 	}
-
 	public static String format(String string, int length) {
 		return String.format("%1$" + length + "s", string);
 	}
+	public static String getSequenceName(String tableName, String columnName) {
+		String sequenceName = tableName + "_" + columnName + "_seq";
+		return sequenceName;
+	}
 
+	
+	/******************************** For Parse Query *******************************/
 	public static String getFilePath(String type, String filename) {
-		String ext = MyDatabase.tableFormat;
+		String extention = MyDatabase.tableFormat;
 		String folder = MyDatabase.userDataFolder;
-		if (type.equals("master"))
+		if (type.equals("master")) {
 			folder = MyDatabase.systemDataFolder;
+		}
 		else if (type.equals("index")) {
 			folder = MyDatabase.indicesFolder;
-		} else if (type.equals("seq")) {
+		} 
+		else if (type.equals("seq")) {
 			folder = MyDatabase.seqFolder;
 		}
 
-		Path path = FileSystems.getDefault().getPath(MyDatabase.tableLocation,
-				folder, filename + ext);
+		Path path = FileSystems.getDefault().getPath(
+				MyDatabase.tableLocation, folder, filename + extention);
 		return path.toString();
 	}
 
@@ -64,69 +71,81 @@ public class Utility {
 		return str.toString();
 	}
 
-	public static String getSequenceName(String tableName, String columnName) {
-		String seqName = tableName + "_" + columnName + "_seq";
-		return seqName;
+	public static LinkedHashMap<String, ArrayList<String>> buildInsertRecord(List<String> values) {
+		LinkedHashMap<String, ArrayList<String>> token = new LinkedHashMap<String, ArrayList<String>>();
+		List<String> columnNames = new ArrayList<String>(Arrays.asList("rowid",
+				"table_name", "column_name", "data_type", "ordinal_position",
+				"is_nullable", "default", "is_unique", "auto_increment"));
+		List<String> dataTypes = new ArrayList<String>(Arrays.asList(
+				INT, TEXT,TEXT, TEXT, TINYINT, TEXT, TEXT, TEXT, TEXT));
+		if (values.size() != columnNames.size())
+			return null;
+		for (int i = 0; i < values.size(); i++) {
+			token.put(columnNames.get(i), 
+					new ArrayList<String>(Arrays.asList(dataTypes.get(i), values.get(i))));
+		}
+		return token;
 	}
 
-	/**
-	 * Validations - Type checkings
-	 */
+	
 	public static boolean checkValue(String type, String value) {
 		switch (type.toLowerCase()) {
 		case TEXT:
 			if ((value.charAt(0) == '\'' && value.charAt(value.length() - 1) == '\'')
-					|| (value.charAt(0) == '"' && value
-							.charAt(value.length() - 1) == '"'))
+				|| (value.charAt(0) == '"' && value.charAt(value.length() - 1) == '"'))
 				return true;
 			break;
 		case TINYINT:
-			if (Integer.parseInt(value) >= Byte.MIN_VALUE
-					&& Integer.parseInt(value) <= Byte.MAX_VALUE)
+			if (Integer.parseInt(value) >= Byte.MIN_VALUE 
+			&& Integer.parseInt(value) <= Byte.MAX_VALUE)
 				return true;
 			break;
 		case SMALLINT:
 			if (Integer.parseInt(value) >= Short.MIN_VALUE
-					&& Integer.parseInt(value) <= Short.MAX_VALUE)
+			&& Integer.parseInt(value) <= Short.MAX_VALUE)
 				return true;
 			break;
 		case INT:
 			if (Integer.parseInt(value) >= Integer.MIN_VALUE
-					&& Integer.parseInt(value) <= Integer.MAX_VALUE)
+			&& Integer.parseInt(value) <= Integer.MAX_VALUE)
 				return true;
 			break;
 		case BIGINT:
 			if (Long.parseLong(value) >= Long.MIN_VALUE
-					&& Long.parseLong(value) <= Long.MAX_VALUE)
+			&& Long.parseLong(value) <= Long.MAX_VALUE)
 				return true;
 			break;
 		case REAL:
 			if (Float.parseFloat(value) >= Float.MIN_VALUE
-					&& Float.parseFloat(value) <= Float.MAX_VALUE)
+			&& Float.parseFloat(value) <= Float.MAX_VALUE)
 				return true;
 			break;
 		case DOUBLE:
 			if (Double.parseDouble(value) >= Double.MIN_VALUE
-					&& Double.parseDouble(value) <= Double.MAX_VALUE)
+			&& Double.parseDouble(value) <= Double.MAX_VALUE)
 				return true;
 			break;
-		case DATETIME:
-			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+		case DATE:
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 			try {
-				if (value.contains("\'"))
+				if (value.contains("\'")) {
 					value = value.replaceAll("\'", "");
-				Date date = df.parse(value);
-			} catch (ParseException e) {
+				}
+				Date date = dateFormat.parse(value);
+			} 
+			catch (ParseException e) {
 				return false;
 			}
 			return true;
-		case DATE:
-			SimpleDateFormat d = new SimpleDateFormat("yyyy-MM-dd");
+		case DATETIME:
+			SimpleDateFormat dateTime = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
 			try {
-				if (value.contains("\'"))
+				if (value.contains("\'")) {
 					value = value.replaceAll("\'", "");
-				Date date = d.parse(value);
-			} catch (ParseException e) {
+				}
+				Date date = dateTime.parse(value);
+			} 
+			catch (ParseException e) {
 				return false;
 			}
 			return true;
@@ -134,25 +153,6 @@ public class Utility {
 			return false;
 		}
 		return false;
-	}
-
-	public static LinkedHashMap<String, ArrayList<String>> buildInsertRecord(
-			List<String> values) {
-		LinkedHashMap<String, ArrayList<String>> token = new LinkedHashMap<String, ArrayList<String>>();
-		List<String> colNames = new ArrayList<String>(Arrays.asList("rowid",
-				"table_name", "column_name", "data_type", "ordinal_position",
-				"is_nullable", "default", "is_unique", "auto_increment"));
-		List<String> dataTypes = new ArrayList<String>(Arrays.asList(INT, TEXT,
-				TEXT, TEXT, TINYINT, TEXT, TEXT, TEXT, TEXT));
-		if (values.size() != colNames.size())
-			return null;
-		for (int i = 0; i < values.size(); i++) {
-			token.put(
-					colNames.get(i),
-					new ArrayList<String>(Arrays.asList(dataTypes.get(i),
-							values.get(i))));
-		}
-		return token;
 	}
 	
 }
